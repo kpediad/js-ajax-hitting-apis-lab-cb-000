1c5dea4cd66ae4de046074d7a9b44aae3ec4a77a
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-26 09:27:23 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function showRepositories() {
   const repos = JSON.parse(this.responseText);
   //console.log(repos);
   const repoList = `<ul>${repos
-    .map(r => '<li><a href="' + r.html_url + '">' + r.name + '</a>' + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a>' + ' - <a href="#" data-repo="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>')
+    .map(r => '<li><a href="' + r.html_url + '">' + r.name + '</a>' + ' - <a href="#" data-username="' + r.owner.login + '" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a>' + ' - <a href="#" data-username="' + r.owner.login + 'data-repo="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>')
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
