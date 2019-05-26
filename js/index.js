0f3adda4783a03d74765d7184f9a3b416527f57f
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-26 08:47:08 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // your code here
+function showRepositories() {
+  //this is set to the XMLHttpRequest object that fired the event
+  console.log(this.responseText);
+}
+ 
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  const username = document.getElementById('username').value;
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/' + username + '/repos');
+  req.send();
+}
