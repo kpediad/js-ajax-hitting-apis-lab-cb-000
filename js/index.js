cd8cc217b901933c71281160520da10d8403c5b5
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-26 09:52:54 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -59,7 +59,7 @@ function displayRepositories() {
 function getRepositories() {
   const req = new XMLHttpRequest();
   const username = document.getElementById('username').value;
-  req.addEventListener('load', showRepositories);
+  req.addEventListener('load', displayRepositories);
   req.open('GET', 'https://api.github.com/users/' + username + '/repos');
   req.send();
 }
