07e7a387a68a339b5cc10593693bdae9a962beba
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-26 09:49:17 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -7,6 +7,8 @@ function displayCommits() {
         '<li><strong>' +
         commit.author.login +
         '</strong> - ' +
+        commit.author.name +
+        ' - ' +
         commit.commit.message +
         '</li>'
     )
