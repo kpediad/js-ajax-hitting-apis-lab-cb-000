61a16471d2d118368173eb287df8aa2b7a62ccee
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-26 09:36:45 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
 // your code here
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
+}
+
 function getCommits(el) {
   const name = el.dataset.repo;
   const username = el.dataset.username;
